e0aba2d6541e88577eda945cbc5cfbe4647b6f1d
Rasheed <abdulrasheed8123@gmail.com>
2022-04-10 16:28:34 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const arr = ["Android","Blackberry","iPhone","Windowa Phone"]
+  const arr1 = ["Samsung","HTC","Micromax","Apple"]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mobile Operating System</h1>
+      <ul>
+        {arr.map((e)=>{
+           return <li>{e}</li>
+        })}
+      </ul>
+      <h1>Mobile Manufacturers</h1>
+      <ul>
+        {arr1.map((e)=>{
+           return <li>{e}</li>
+        })}
+      </ul>
+
     </div>
   );
 }
