8b987b8c49fcaf2b2c20b0ddd72d9de54949f133
Rasheed <abdulrasheed8123@gmail.com>
2022-04-12 20:41:38 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import { Navbar } from './components/navbar'
+import "./App.css";
+import { Inventory } from "./components/Inventory";
 
 function App() {
-  const arr =  ["Android","Blackberry","iPhone","Windowa Phone"]
-
-  const arr1 = ["Samsung","HTC","Micromax","Apple"]
   return (
     <div className="App">
-    <h1>Mobile Operating System</h1>
-    <ul>{arr.map((e)=>{
-      return <li>{e}</li>
-    })}</ul>
-    <h1>Mobile Manufacturers</h1>
-    <ul>{arr1.map((e)=>{
-      return <li>{e}</li>
-    })}</ul>
-    <Navbar />
+      <Inventory />
     </div>
-    
   );
 }
 
